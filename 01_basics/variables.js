bf8379fb5dfe2e243b63f54b3f1b00b4e556984a
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2025-01-16 16:03:52 +0000

    Variables commit

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 65783;
+var personName = "Vishal";
+let country = "India"
+state = "Rajasthan"
+let district;
+
+
+console.log(personName);
+console.table([accountId,personName,country,state,district]);
