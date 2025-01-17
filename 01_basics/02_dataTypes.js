08fd54a15b27e7a9de5c23ba5f7fd962e0405a2f
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2025-01-17 10:05:09 +0000

    data types commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+//"use strict  " // treat all js code as a newer version
+
+//alert (3+3) // We are using node js not browser
+
+console.log(3+
+    3);
+
+    //code readabilty that should be high
+
+ console.log("Vishal");
+
+ let name = "vishal"
+ let age = 25;
+ let isLoggedIn = true;
+ let state = null;
+
+ // number => 2 to power 53
+ // bigint
+ // string
+ // boolean
+ // null => standalone value
+ // undefined =>
+ // symbol
+
+
+ // object
+
+ console.log(typeof age)
+ console.log(typeof null);
+ console.log(typeof undefined);
