4f189b78bbf4ba7789f0322910d9a7ff6e501f4b
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2025-01-30 12:27:25 +0000

    conversion operations commit

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,2 +1,24 @@
 let score = 33
-console.log(score)
+console.log(typeof score)
+console.log(typeof (score))
+console.log(typeof Number(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+let name = "";
+console.log(typeof name)
+
+// 1 => true;  0 => false;
+// "" => false;
+// "hitesh" => true
+
+let someNumber = 18;
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
