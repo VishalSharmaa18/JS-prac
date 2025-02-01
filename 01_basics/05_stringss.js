dc3a28427d795a49e8b650b266ea7fe7a0f6128c
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2025-02-01 16:59:39 +0000

    string in js commit

---
+++ 01_basics/05_stringss.js
@@ -0,0 +1,10 @@
+const name = "Vishal";
+const repoCount = 50;
+
+//console.log(name + ' '+ repoCount) this is a bad way of doing things..
+
+console.log(`my name was ${name} and my repocount was ${repoCount}`);
+
+
+const myName = new String("Virat Kohli")
+console.log(myName[1]);
