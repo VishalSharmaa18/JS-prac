d613d770aad2b22cc427406f27662a46def19915
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2025-02-02 10:21:35 +0000

    strings

--- 01_basics/05_stringss.js
+++ 01_basics/05_stringss.js
@@ -8,3 +8,9 @@ console.log(`my name was ${name} and my repocount was ${repoCount}`);
 
 const myName = new String("Virat Kohli")
 console.log(myName[1]);
+
+console.log(myName.charAt(3));
+console.log(myName.indexOf('V'));
+
+
+
