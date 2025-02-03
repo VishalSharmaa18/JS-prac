6afbb9c34096a2557245c24a7b99a2fcaec729c8
VishalSharma <152493625+VishalSharmaa18@users.noreply.github.com>
2025-02-03 10:16:24 +0000

    strings in js commit

--- 01_basics/05_stringss.js
+++ 01_basics/05_stringss.js
@@ -12,5 +12,16 @@ console.log(myName[1]);
 console.log(myName.charAt(3));
 console.log(myName.indexOf('V'));
 
+const newString = myName.substring(1,4)
+console.log(newString);
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+console.log(str.slice(31));
+console.log(str.slice(1,10));
+console.log(str.slice(-1,-5));
+
+const str2= "Hellt World";
+console.log(str2.slice(-5, -1));
+
 
 
